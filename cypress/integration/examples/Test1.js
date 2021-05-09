30504b39c3ecb9b17db2b3dc39e8713902af1b83
saradrada <saraodrada@gmail.com>
2021-05-09 17:58:32 -0500

    add alias to test 1

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -6,19 +6,25 @@ describe('My First Test', () => {
         cy.get('.search-keyword').type('ca')
         cy.get('.product:visible').should('have.length', 4)
 
+        // Alias
+        cy.get('.products').as('product-locator')
+
         // Parent child chaining
+        cy.get('@product-locator').find('.product').should('have.length', 4)
+        cy.get('@product-locator').find('.product').eq(2).contains('ADD TO CART').click()
 
-        cy.get('.products').find('.product').should('have.length', 4)
-        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
-
-        cy.get('.products').find('.product').each(($el, index, $list) => {
+        cy.get('@product-locator').find('.product').each(($el, index, $list) => {
             if ($el.find('.product-name').text().includes('Capsicum')) {
                 cy.wrap($el).contains('ADD TO CART').click()
             }
         })
 
+        // Pring log
         cy.get('.brand').then((logo) => {
             cy.log(logo.text())
         })
+
+        // Assert logo text is displayed correctly
+        cy.get('.brand').should('have.text', 'GREENKART')
     })
 })
