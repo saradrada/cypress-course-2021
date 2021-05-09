3865ea0e47fda0b12768d1a7ac3bdd376e78b9fb
saradrada <saraodrada@gmail.com>
2021-05-09 15:09:00 -0500

    add first tests

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,5 +1,24 @@
+/// <reference types="Cypress" />
+
 describe('My First Test', () => {
     it('Does not do much!', () => {
         cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+        cy.get('.search-keyword').type('ca')
+        cy.get('.product:visible').should('have.length', 4)
+
+        // Parent child chaining
+
+        cy.get('.products').find('.product').should('have.length', 4)
+        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+
+        cy.get('.products').find('.product').each(($el, index, $list) => {
+            if ($el.find('.product-name').text().includes('Capsicum')) {
+                cy.wrap($el).contains('ADD TO CART').click()
+            }
+        })
+
+        cy.get('.brand').then((logo) => {
+            cy.log(logo.text())
+        })
     })
 })
